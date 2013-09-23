93384436a69baf809e54a52b9020917bb23b015a
Hassan Amouhzi <github@anezi.net>
2013-09-23 15:37:30 +0200

    Relative Url bug

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -4,7 +4,7 @@ class AttachmentUploader < CarrierWave::Uploader::Base
   storage :file
 
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    Gitlab.config.gitlab.relative_url_root + "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   def image?
